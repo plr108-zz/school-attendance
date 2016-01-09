4462bfb18900d9dcf6b5bbc10bf897d0a6f30995
Patrick Roche <patrick.l.roche@gmail.com>
2016-01-09 10:07:38 -0500

    Implement octopus.getLocalStorage()

--- js/app.js
+++ js/app.js
@@ -27,11 +27,137 @@
 }());
 
 
+var model = {
+    numStudents : 7,
+    numDays: 12,
+    students : [
+        {
+            name: 'Kano',
+            id: 'kano',
+            daysMissed: [1,3,5,7]
+        },
+        {
+            name: 'Liu Kang',
+            id: 'liu-kang',
+            daysMissed: [2,8]
+        },
+        {
+            name: 'Raiden',
+            id: 'raiden',
+            daysMissed: [4,7,9],
+        },
+        {
+            name: 'Johnny Cage',
+            id: 'johnny-cage',
+            daysMissed: [1,2,3,4,5]
+        },
+        {
+            name: 'Scorpion',
+            id: 'scorpion',
+            daysMissed: [2]
+        },
+        {
+            name: 'Sub Zero',
+            id: 'sub-zero',
+            daysMissed: [2]
+        },
+        {
+            name: 'Sonya',
+            id: 'sonya',
+            daysMissed: [8,1]
+        }
+    ]
+};
+
+var view = {
+
+    init: function() {
+        this.tableHead = $('#table-head');
+        this.tableBody = $('#table-body');
+        this.render();
+    },
+
+    render: function() {
+        this.createHeaderRow();
+        this.createStudentRows();
+    },
+
+    createHeaderRow: function() {
+        var numDays = octopus.getNumDays();
+        var htmlStr = '<tr><th class="name-col">Student Name</th>';
+
+        for(var i = 1; i <= numDays; i ++) {
+            htmlStr += '<th>' + i + '</th>';
+        }
+
+        htmlStr += '<th class="missed-col">Days Missed</th></tr>';
+        this.tableHead.append(htmlStr);
+    },
+
+    createStudentRows: function() {
+        var students = octopus.getStudents();
+        for (i = 0; i < students.length; i ++) {
+
+            var student = students[i];
+            var htmlStr = '<tr class="student" id=' + student.id + '><td class="name-col">' + student.name + '</td>';
+
+            for (j = 0; j < octopus.getNumDays(); j++) {
+                htmlStr += '<td class="attend-col"><input type="checkbox"';
+                if(!student.daysMissed.includes(j+1)) {
+                    htmlStr += ' checked';
+                }
+
+                htmlStr += '></td>';
+
+            }
+
+            htmlStr += '<td class="missed-col">' + student.daysMissed.length + '</td></tr>';
+
+            this.tableBody.append(htmlStr);
+        }
+    },
+
+    countMissing: function() {
+    }
+};
+
+var octopus = {
+
+    init : function() {
+        view.init();
+    },
+
+    getNumStudents : function() {
+        return model.numStudents;
+    },
+
+    getNumDays : function() {
+        return model.numDays;
+    },
+
+    getStudents : function() {
+        return model.students;
+    },
+
+    getLocalStorage : function() {
+
+        var attendance = JSON.parse(localStorage.attendance);
+
+        return attendance;
+    }
+};
+
 /* STUDENT APPLICATION */
 $(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+
+    octopus.init();
+
+    var attendance = octopus.getLocalStorage();
+
+    $allMissed = $('tbody .missed-col');
+    $allCheckboxes = $('tbody input');
+
+    view.countMissing();
 
     // Count a student's missed days
     function countMissing() {
@@ -81,4 +207,6 @@ $(function() {
     });
 
     countMissing();
+
+
 }());
